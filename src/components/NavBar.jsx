8eed7f4599a71d0eb506ad462174aae4cf4c3270
candeaquiles <80926300+candeaquiles@users.noreply.github.com>
2021-09-28 21:31:05 -0300

    Add files via upload

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function NavBar(){
+
+    return(
+                 <nav>
+         <h1 className="titleOne">Atenas Fitness</h1>
+         <ul>
+             <li>Inicio</li>
+             <li>Productos</li>
+             <li>Sobre nosotros</li>
+             <li>Contacto</li>
+         </ul>
+     </nav>
+
+    )
+}
+
+
+export default NavBar;
